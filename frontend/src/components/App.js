40c1912f3294b0626cac9e092ad67d926f071f44
Olha Romanova <larklarky@gmail.com>
2021-02-20 13:37:04 +0100

    add footer, design changes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -84,6 +84,19 @@ class App extends Component {
             </Route>
             
           </Switch>
+
+            <div class="footer-basic">
+              <footer>
+                  <div class="social"><a href="#"><i class="icon ion-social-instagram fa fa-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat fa fa-snapchat-ghost"></i></a><a href="#"><i class="icon ion-social-twitter fa fa-twitter"></i></a><a href="#"><i class="icon ion-social-facebook fa fa-facebook"></i></a></div>
+                  <ul class="list-inline">
+                      <li class="list-inline-item"><a href="#">Home</a></li>
+                      <li class="list-inline-item"><a href="#">About</a></li>
+                      <li class="list-inline-item"><a href="#">Terms</a></li>
+                      <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                  </ul>
+                  <p class="copyright">Company Name © 2018</p>
+              </footer>
+          </div>
         </div>
       </Router>
     )
