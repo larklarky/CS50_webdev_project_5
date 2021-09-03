d4c5c47da3df95482f49edfe9c743db7934ef97c
Olha Romanova <larklarky@gmail.com>
2021-09-03 16:04:37 +0200

    add tests for authorization

--- backend/fanfiction/tests.py
+++ backend/fanfiction/tests.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
+from django.http import response
 from django.test import TestCase, Client
-from .models import FandomCategory
-from .serializers import FandomCategorySerializer
+from .models import FandomCategory, User
+from .serializers import FandomCategorySerializer, UserSerializer
 from django.urls import reverse
 from rest_framework import status
 
@@ -36,3 +38,51 @@ class FandomCategoryTest(TestCase):
         serializer = FandomCategorySerializer(fandom_category)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class AuthorizationTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='user1', password='1111', email='user1@mail.com')
+    
+    def test_login(self):
+        response = client.put(
+            '/api/users/' + str(self.user1.id) + '/',
+            {'first_name': 'Alex', 'last_name¶': 'DJ'},
+            content_type= 'application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        response = client.post('/api/token/', {'username': 'user1', 'password': '1111' }, content_type= 'application/json')
+        self.assertContains(response, 'token')
+        
+        token = "Token " + response.data['token']
+        response = client.patch(
+            '/api/users/' + str(self.user1.id) + '/',
+            {'first_name': 'Alex', 'last_name¶': 'DJ'},
+            content_type= 'application/json',
+            HTTP_AUTHORIZATION=token
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_signUp(self):
+        response = client.post(
+            '/api/registration/', 
+            {'username':'user2', 'password':'1111', 'email':'user2@mail.com'}, 
+            content_type= 'application/json')
+
+        self.assertContains(response, 'id', status_code=status.HTTP_201_CREATED)
+        self.assertEqual(response.data['username'], 'user2')
+        self.assertEqual(response.data['email'], 'user2@mail.com')
+
+        response = client.post('/api/token/', {'username': 'user2', 'password': '1111' }, content_type= 'application/json')
+        self.assertContains(response, 'token')
+        
+        token = "Token " + response.data['token']
+        response = client.patch(
+            '/api/users/' + str(self.user1.id) + '/',
+            {'first_name': 'Alex', 'last_name¶': 'DJ'},
+            content_type= 'application/json',
+            HTTP_AUTHORIZATION=token
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
